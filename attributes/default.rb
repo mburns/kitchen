4c92f62e12bcc1422457544e1ac422929ea12414
Michael Burns <5170+mburns@users.noreply.github.com>
2019-09-18 16:09:08 -0700

    bump package version to match the statsd project release tag (#10)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['statsd']['port'] = 8125
 default['statsd']['graphite_port'] = 2003
 default['statsd']['graphite_host'] = 'localhost'
-default['statsd']['package_version'] = '0.6.0'
+default['statsd']['package_version'] = '0.8.0'
 default['statsd']['sha'] = '972993ef59c34ab92883bc62271efbc90514dde3'
 default['statsd']['user'] = 'statsd'
 default['statsd']['repo'] = 'https://github.com/etsy/statsd.git'
